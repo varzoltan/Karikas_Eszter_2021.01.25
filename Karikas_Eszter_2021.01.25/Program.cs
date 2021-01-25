3f339815880d68678de301ee9d78cbeb89fb5f57
varzoltan <varzoltan@tanarium.hu>
2021-01-25 10:09:47 +0100

    Add project files.

---
+++ Karikas_Eszter_2021.01.25/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karikas_Eszter_2021._01._25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
